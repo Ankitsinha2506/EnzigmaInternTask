c89fe47c11addccfcc6a315835ef17443befef0a
Ankit8210 <ankitsinha25061909@gmail.com>
2024-08-11 09:54:09 +0530

    Connected to MongoDB databases.

---
+++ mean-todo-app/backend/models/task.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description:{
+        type: String
+    },
+    
+})
---
+++ mean-todo-app/backend/server.js
@@ -0,0 +1,45 @@
+import express from 'express'
+// import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+
+const PORT = 4000;
+
+
+// dotenv.config();
+
+// const PORT =  process.env.PORT || 5000 ;
+// const URI = process.env.MongoDBURI;
+
+// Connect to MongoDB.
+mongoose.connect(
+    'mongodb://localhost:27017/mean-todo-app', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(()=>console.log("MongoDb Connected...."))
+.catch((err)=>console.log("Connection error : "+err)
+)
+
+// Connect to MongoDB.
+/*
+try {
+    mongoose.connect(URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    console.log("Connected to MongoDB");
+    
+} catch (error) {
+    console.log("Error Connecting to MongoDB " + error);
+}
+*/
+
+// App listening.
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
