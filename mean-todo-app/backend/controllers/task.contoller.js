600fb0931a3f17fc127a8bc4d4a5461658a4c162
Ankit8210 <ankitsinha25061909@gmail.com>
2024-08-11 11:56:24 +0530

    update and delete APIs done.

--- mean-todo-app/backend/controllers/task.contoller.js
+++ mean-todo-app/backend/controllers/task.contoller.js
@@ -1,29 +1,57 @@
 import Task from "../models/task.model.js";
 
 // Fetch All Task.
-export const getTask = async(req, res)=>{
+export const getTask = async (req, res) => {
     try {
         const task = await Task.find();
-        res.status(200).json(task)
+        res.status(200).json({message: "Data fetched Successfully", task})
 
     } catch (error) {
-        console.log("Error: "+error);
-        res.status(500).json({message: error.message})   
+        console.log("Error: " + error);
+        res.status(500).json({ message: error.message })
     }
 }
 
 // Add Task.
-export const addTask = async(req, res)=>
-    {
+export const addTask = async (req, res) => {
 
     try {
-        const {title, description, status, createdAt, updatedAt} = req.body;
-        const newTask = new Task({title, description, createdAt, updatedAt});
+        const { title, description, status, createdAt, updatedAt } = req.body;
+        const newTask = new Task({ title, description, createdAt, updatedAt });
         await newTask.save();
-        res.status(201).json({message: "Task Added Successfully", newTask});
+        res.status(201).json({ message: "Task Added Successfully", newTask });
     }
     catch (error) {
-        console.log("Error: "+error);
+        console.log("Error: " + error);
         res.status(500).json({ error: "An error occurred while adding the task" });
     }
+}
+
+//APIs for update task.
+export const updateTask = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { title, description, status, createdAt, updatedAt } = req.body;
+        const task = await Task.findByIdAndUpdate(id, { title, description, status, createdAt, updatedAt },
+            { new: true });
+        res.status(200).json({ message: "Task Updated Successfully", task });
+    }
+    catch (error) {
+        console.log("Error: " + error);
+        res.status(500).json({ error: "An error occurred while updating the task" });
+    }
+}
+
+// APIs for Delete Task.
+export const deleteTask = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const task = await Task.findByIdAndDelete(id);
+        res.status(200).json({ message: "Task Deleted Successfully", task});
+    }
+    catch (error) {
+        console.log("Error: " + error);
+        res.status(500).json({ error: "An error occurred while deleting the task." })
+    }
+   
 }
--- mean-todo-app/backend/routes/taskRouter.js
+++ mean-todo-app/backend/routes/taskRouter.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { addTask, getTask } from '../controllers/task.contoller.js';
+import { addTask, deleteTask, getTask, updateTask } from '../controllers/task.contoller.js';
 
 const router = express.Router()
 
 router.get("/", getTask);
 router.post("/", addTask);
+router.put('/edit/:id', updateTask);
+router.delete("/edit/:id", deleteTask)
 
 
 export default router;
